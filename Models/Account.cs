5f95f579883056b229aabae78f8507e03c7463e2
Timka6434 <timka6434@mail.ru>
2024-09-23 23:45:06 +0500

    Initial commit

---
+++ Models/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketFinansistWPF.Models
+{
+     public partial class Account
+    {
+        private string _id {  get; set; }
+        private string _accountName { get; set; }
+        private decimal _balance { get; set; }
+        private bool _isLocked { get; set; }
+        private DateTime _DataCreateAccount {  get; set; }
+
+        public Account(string name) 
+        {
+            _id = Guid.NewGuid().ToString();
+            _accountName = name;
+            _balance = 0;
+            _isLocked = true;
+            _DataCreateAccount = DateTime.Now;
+        }
+        public string GetAccountName() => _accountName;
+        public void SetAccountName(string SetName) => _accountName = SetName;
+
+        public void SetBalance(decimal  Balance) => _balance = Balance;
+
+        public void UnlockAccount() => _isLocked = false;
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PocketFinansistWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace WpfApp3.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private Account _currentAccount;
+
+        [ObservableProperty]
+        private string _accountNameInput;
+        
+        [ObservableProperty]        
+        ObservableCollection<Account> _accounts = new ObservableCollection<Account>();
+
+
+        public ICommand NavigateCommand { get; }
+
+        public MainViewModel()
+        {
+            NavigateCommand = new RelayCommand<object>(Navigate);
+        }
+
+        private void Navigate(object parameter)
+        {
+            if (parameter is NavigationService navigationService)
+            {
+                navigationService.Navigate(new Uri("Views/Page1.xaml", UriKind.Relative));
+            }
+        }
+
+        [RelayCommand]
+        private void ActivateAccount()
+        {
+            if (CurrentAccount == null)
+            {
+                MessageBox.Show("Аккаунт не создан!");
+                return;
+            }
+
+            if (CurrentAccount.GetAccountName() != null)
+            {
+                CurrentAccount.UnlockAccount();
+            }
+        }
+
+        [RelayCommand]
+        public void CreateAccount()
+        {
+            if(string.IsNullOrWhiteSpace(AccountNameInput))
+            {
+                MessageBox.Show("Введите корректное имя для аккаунта!");
+                return;
+            }
+            CurrentAccount = new Account(AccountNameInput);
+
+            Accounts.Add(CurrentAccount);
+        }
+        
+    }
+}
